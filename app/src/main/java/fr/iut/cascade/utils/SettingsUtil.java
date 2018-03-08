6f149bdce8941fc835431cba8566c1d5e45c73d2
Lilian Gallon <lilian.gallon@etu.u-bordeaux.fr>
2018-03-08 19:15:21 +0100

    Added the french translation

--- app/src/main/java/fr/iut/cascade/utils/SettingsUtil.java
+++ app/src/main/java/fr/iut/cascade/utils/SettingsUtil.java
@@ -6,6 +6,8 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import fr.iut.cascade.R;
+
 /**
  * This file is part of Cascade.
  *
@@ -49,7 +51,7 @@ public class SettingsUtil {
 
         // It isn't a best score we don't save it obviously
         if(!place_found && score_list.size() > 10){
-            Toast.makeText(app_context, "Vous n'avez battu aucun de vos record", Toast.LENGTH_SHORT).show();
+            Toast.makeText(app_context, R.string.not_record, Toast.LENGTH_SHORT).show();
             return;
         }
 
